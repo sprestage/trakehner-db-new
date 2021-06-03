8c17b8fd60e2b2bb8cb6aa9c3f636f5ed79bb29c
Susan Prestage <susan@sportsrocket.com>
2021-06-03 15:27:59 -0500

    Implement basic frame of app with images and stylesheets.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-    root 'home#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/contact' => 'static#contact'
+  get '/about' => 'static#about'
+
+  resources :breeders
+
+  resources :horses do
+    member do
+      get 'progeny'
+    end
+  end
+  
+  root to: 'home#index'
+
 end
