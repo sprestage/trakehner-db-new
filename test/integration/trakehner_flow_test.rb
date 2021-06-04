2ac8a6ece4fa190ef5178cd5e10761b37b029d6e
Susan Prestage <susan@sportsrocket.com>
2021-06-03 20:37:10 -0500

    Add testing framework with basic tests.

---
+++ test/integration/trakehner_flow_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class TrakehnerFlowTest < ActionDispatch::IntegrationTest
+  test "Home page contains text" do
+    visit root_path
+    assert_includes(body, "Horse")
+  end
+
+  test "Home page has a correct title" do
+    visit root_path
+    assert_title "The Unofficial Trakehner Database"
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,29 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
-require 'rails/test_help'
+require 'capybara/rails'
+require 'capybara/minitest'
+require 'pry'
+
+class ActionDispatch::IntegrationTest
+  # Make the Capybara DSL available in all integration tests
+  include Capybara::DSL
+  # Make `assert_*` methods behave like Minitest assertions
+  include Capybara::Minitest::Assertions
+
+  # Reset sessions and driver between tests
+  teardown do
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+  end
+end
+
+ENV["RAILS_ENV"] = "test"
+require File.expand_path("../../config/environment", __FILE__)
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  # fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
 end
